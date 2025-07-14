aaa43aeca134c4715d32c8ff16431bcb7f75e06f
kimpdlcgh <kimpdlc24@gmail.com>
2025-07-14 16:47:35 +0800

    Fix missing useEffect import in SignupSuccess

--- src/pages/SignupSuccess.tsx
+++ src/pages/SignupSuccess.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Wallet, BarChart3, ArrowRight, CheckCircle, TrendingUp } from 'lucide-react';
 import { Button } from '../components/ui/Button';
